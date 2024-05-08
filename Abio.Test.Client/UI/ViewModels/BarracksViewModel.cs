3c798ffce9a72780f24bc9c54889f3d9caaa74e1
bridgerwinter <bridgerwinter@gmail.com>
2024-05-07 22:17:10 -1000

    get all units from barracks.

--- Abio.Test.Client/UI/ViewModels/BarracksViewModel.cs
+++ Abio.Test.Client/UI/ViewModels/BarracksViewModel.cs
@@ -1,19 +1,53 @@
-﻿using System;
+﻿using Abio.Library.DatabaseModels;
+using Abio.Library.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Abio.Test.Client.UI.ViewModels
 {
-    class BarracksViewModel : INotifyPropertyChanged
+    public class BarracksViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged(string propertyName)
+        public virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
+
+        public BarracksViewModel()
+        {
+            InitializeAsync();
+        }  
+        public async void InitializeAsync()
+        {
+            var units = await ApiService.GetAllUnits();
+            this.AllUnits = new ObservableCollection<Unit>(units);
+        }
+
+        public ObservableCollection<Unit> AllUnits
+        {
+            get
+            {
+                return _allUnits;
+            }
+            set
+            {
+                _allUnits = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Unit> _allUnits = new ObservableCollection<Unit>();
+
     }
 }
